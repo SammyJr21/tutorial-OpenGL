f2c1f9b6eca576fa749f186843b1ccb27c70f579
Sammy Teixeira Guimarães Junior <sammyjuniorgt@gmail.com>
2022-11-30 01:22:33 -0300

    Utilizando o Glad para alterar a cor da tela Implementacao de um funçao callback para acompanhar re-size

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+#include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
 using std::cout;
 using std::endl;
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height); // Protótipo de função
 
 const unsigned int WIDTH = 800; 
 const unsigned int HEIGHT = 600;
@@ -12,17 +15,42 @@ int main()
     // Inicializar biblioteca GLFW
     glfwInit();
 
+    // Especificar versão do OpenGL
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // Instanciar GLFWwindow
-    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Tutorial OpenGl", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Tutorial OpenGL", NULL, NULL);
     if (window == NULL)
     {
         cout << "Falha ao criar GLFWwindow!" << endl;
+        return -1;
+    }
+    // Definir window como contexto atual da thread de chamada
+    glfwMakeContextCurrent(window);
+    // Definir função de retorno de chamada para redimensionamento de janela
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    // Verificar GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Nao foi possivel carregar o contexto OpenGL!" << endl;
         return -1;
     }
 
     // Loop de renderização principal
     while (!glfwWindowShouldClose(window))
     {
+        // Especificar os valores de vermelho, verde, azul e alfa, para limpar os buffers de cores
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        // Definir qual ou quais buffers precisam ser limpos
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Trazer os "back buffers" para frente
+        glfwSwapBuffers(window);
+
         // Processar todos os eventos pendentes
         glfwPollEvents();
     }
@@ -32,3 +60,8 @@ int main()
 
     return 0;
 } 
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
